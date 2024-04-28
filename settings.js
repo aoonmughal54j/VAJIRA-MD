4b13bcc07a3a430cd61b53c70eba6d5e5d0d9fb8
aoonmughal54j <157740621+aoonmughal54j@users.noreply.github.com>
2024-04-28 20:57:02 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,22 @@ function convertToBool(text, fault = 'true') {
 global.email = 'vajirarathnayaka891@gmail.com'
 global.github = 'https://github.com/VajirTech/VAJIRA-MD'
 global.location = 'Srilanka Colombo'
-global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
+global.packname = '𓆩J E E R Y 𓆪
+ *
+ *
+ *
+ *
+ *
+ *
+ <<<03185168817
+ *
+ *
+ *
+ *
+ *
+ * 
+>>>>𝗧𝗛𝗘 [𝗝𝗘𝗘𝗥𝗬]';
+global.author = '✦-𝗧𝗛𝗘>𝗝 𝗘 𝗘 𝗥 𝗬^>';
 global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
@@ -22,32 +36,32 @@ global.mess = {
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 }
 
-global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = [process.env.OWNER_NUMBER] || ['923185168817'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
-PREFIX:  process.env.PREFIX || ['.'] ,
+PREFIX:  process.env.PREFIX || ['*'] ,
 LANG:  process.env.LANG || 'SI' ,    
-ANTI_BAD: process.env.ANTI_BAD || false  ,
-AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
-AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
-AUTO_RECORDING:  process.env.AUTO_RECORDING  || false  ,
+ANTI_BAD: process.env.ANTI_BAD || true  ,
+AUTO_REACT:  process.env.AUTO_REACT  || true  ,    
+AUTO_TYPING:  process.env.AUTO_TYPING  || true  ,
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,
 AUTO_READ:  process.env.AUTO_READ  || false  ,
-AUTO_BIO:  process.env.AUTO_BIO  || false  ,    
-ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
-WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
-ANTI_LINK: process.env.ANTI_LINK || false  ,
+AUTO_BIO:  process.env.AUTO_BIO  || true  ,    
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || true  ,            
+WORK_TYPE: process.env.WORK_TYPE || 'public'  ,
+ANTI_LINK: process.env.ANTI_LINK || true  ,
 ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94719199757'  ,    
+OWNER_NUMBER: process.env.OWNER_NUMBER || '923185168817'  ,    
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'Vajira' ,     
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
-AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
+OWNER_NAME: process.env.OWNER_NAME || '𝗧𝗛ع𝗲 𝗝 𝗘 𝗘 𝗥 𝗬' ,     
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
 AUTO_STICKER: process.env.AUTO_STICKER || false  ,
 WELCOME:  process.env.WELCOME  || false ,    
 NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || false  ,
 FOOTER: process.env.FOOTER ||  '©ＶＡＪＩＲＡ - ＭＤ - Ｖ4',
-LOGO: process.env.LOGO || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg` ,
-IMAGE: process.env.IMAGE || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg`     
+LOGO: process.env.LOGO || `https://telegra.ph/file/fa7b7126fad252c23770b.jpg` ,
+IMAGE: process.env.IMAGE || `https://telegra.ph/file/fa7b7126fad252c23770b.jpg`     
 };
